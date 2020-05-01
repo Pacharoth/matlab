258ac8c2a1ef7221774eec92c58ff789a6bfc5ff
Pacharoth <pacharothroeun01@gmail.com>
2020-05-02 05:55:01 +0700

    Sixxxxxxxxxxxxxxxxxxxxxxxxxxxxxth vereison

--- TP1/ex3.m
+++ TP1/ex3.m
@@ -7,11 +7,11 @@ diagonal=[1; 2; 3; 4; 5; 4; 3; 2; 1]
 bMat=zeros(9)+diag(diagonal)
 %c. cMat contains 10 *10 one column 1-10 other column 11-20 so on
 a=[1:1:10; 11:1:20];
-b=[31:1:40; 21:1:30];
+b=[21:1:30;31:1:40 ];
 c=[ 41:1:50; 51:1:60];
 d=[61:1:70; 71:1:80]; 
 e=[81:1:90;91:1:100];
-cMat=[a;b;c;e]'
+cMat=[a;b;c;d;e]'
 %d. dMat contains matric 3*4 use nan
 dMat=nan(3,4)
 %e.eMat=[13 -1 5;-22 10 -87]
